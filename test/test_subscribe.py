f0c004fbd84c1997b0b9297046934c6e2f00bbf2
Tom CzHen <tom.czhen@gmail.com>
2019-06-06 05:38:38 +0800

    编写配置格式测试用例

--- test/test_subscribe.py
+++ test/test_subscribe.py
@@ -1,31 +1,33 @@
 import asyncio
 import base64
 from unittest import TestCase
-
+from typing import List
 from SimpleShadowsocksSubscribeServer.models import SubscribeSchema, Subscribe
 from SimpleShadowsocksSubscribeServer.utils import aio_toml_loads, aio_base64_urlsafe_encode
-from test import SHADOWSOCKS_PATH
+from test import SUBSCRIBE_PATH
 
 
 class TestSubscribe(TestCase):
-    toml_content: dict = asyncio.run(aio_toml_loads(SHADOWSOCKS_PATH))
-    subscribe_uid = '82bce78f-a084-48ac-9cbf-47693e0d0945'
-    subscribe_content = toml_content.get(subscribe_uid)
-    subscribe_schema = SubscribeSchema()
-    subscribe: Subscribe = subscribe_schema.load(subscribe_content).data
+    toml_content: dict = asyncio.run(aio_toml_loads(SUBSCRIBE_PATH))
+    subscribes_toml = toml_content.get('subscribe')
 
     def test_subscribe_shadowsocks(self):
-        shadowsocks_uri_list = [
-            f"ss://{s['method']}:{s['password']}@{s['server']}:{s['server_port']}#{s['remark']}"
-            for s in self.subscribe_content['shadowsocks']
-        ]
-        self.assertEqual(len(self.subscribe.shadowsocks), len(shadowsocks_uri_list))
+        subscribes: List[Subscribe] = [SubscribeSchema().load(_).data for _ in self.subscribes_toml]
+        subscribes_dict = {s['uid']: {'token': s['token'], 'shadowsocks': s['shadowsocks']} for s in
+                           self.subscribes_toml}
+        for subscribe in subscribes:
+            subscribe_dict = subscribes_dict[subscribe.uid]
 
-        self.assertListEqual(
-            [s.uri for s in self.subscribe.shadowsocks],
-            shadowsocks_uri_list
+            shadowsocks_uri_list = [
+                f"ss://{s['method']}:{s['password']}@{s['server']}:{s['server_port']}#{s['remark']}"
+                for s in subscribe_dict['shadowsocks']
+            ]
+            self.assertEqual(len(subscribe.shadowsocks), len(shadowsocks_uri_list))
 
-        )
+            self.assertListEqual(
+                [s.uri for s in subscribe.shadowsocks],
+                shadowsocks_uri_list
+            )
 
     def test_aio_base64_urlsafe_encode(self):
         plain_text = 'test_plain_text'
